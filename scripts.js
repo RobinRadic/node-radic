72921f4bd7e7a8312fbead953d1fd0614173c91a
Robin Radic <robin@radic.nl>
2014-12-24 02:39:31 +0100

    adjustments

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var path = require('path'),
     exec = require('child_process').exec,
+    async = require('async'),
     _ = require('lodash'),
     fs = require('fs'),
     fse = require('fs-extra');
@@ -16,4 +17,38 @@ if (args[2] === 'jsdoc2') {
     fs.writeFileSync(path.join(__dirname, '_README.md'), jsdocReadme, 'utf-8');
     exec('jsdoc -c ./jsdoc.json ./_README.md');
     //fs.unlinkSync(path.join(__dirname, '_README.md'));
+}
+
+
+if (args[2] === 'publish') {
+    var type = args[3]; // major, minor, patch
+    var version;
+
+    async.waterfall([
+        function(next){
+            exec('npm version ' + type, function(err, stdin, stdout){
+                next(err);
+            });
+        },
+        function(next){
+            var pkg = fse.readJsonFileSync(path.join(__dirname, 'package.json'));
+            version = pkg.version;
+            exec('git push -u origin v' + version, function(err, stdin, stdout){
+                next(err);
+            });
+        },
+        function(next){
+            exec('commit v' + version, function(err, stdin, stdout){
+                next(err);
+            });
+        },
+        function(next){
+            exec('npm publish', function(err, stdin, stdout){
+                next(err);
+            });
+        }
+    ], function(err, result){
+        if(err) throw new Error(err);
+        console.log('done');
+    });
 }
