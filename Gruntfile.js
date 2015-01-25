dcfd704a90e633e4ba252ca14fe0a0a8b5dbb434
robinradic <robin@radic.nl>
2015-01-25 10:33:17 +0100

    fixed model and db

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,36 +54,6 @@ module.exports = function (grunt) {
 
     grunt.registerTask('docs', ['radic_jsdoc:docs', 'radic_jsdoc_mdpages:docs',  'git:docs']);
 
-    var cfgold = {
-        radic_jsdoc: {
-            docs: {
-                tutorials: true
-            }
-        },
-        radic_coverage: {
-            docs: {
-                options: {
-                    exclude: ['lib/cli/_celeri/**'],
-                    frontMatterPath: 'docs/processed-front-matter.yml'
-                }
-            }
-        },
-        radic_ghpages_publish: {
-            docs: {
-                options: {
-                    dir: 'docs',
-                    indexName: 'index.md',
-                    frontMatterPath: 'docs/processed-front-matter.yml',
-                    readmePath: 'README.md',
-                    replacer: '# radic'
-                }
-            }
-        }
-    };
-    grunt.registerTask('publish_old', function () {
-        grunt.task.run(['radic_jsdoc:docs']);
-        fs.copySync(path.join(__dirname, 'tutorials'), path.join(__dirname, 'docs/tutorials'), null, true);
-        grunt.task.run(['radic_coverage:docs', 'radic_ghpages_publish:docs']);
-    });
+    grunt.registerTask('default', []);
 
 };
